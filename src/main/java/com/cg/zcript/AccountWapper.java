fb70cf6f66af292104b1cc0a55e91687f26b2096
LoveMan <zhang520godlin@163.com>
2018-11-15 17:54:28 +0800

    bulk api and jdbc

---
+++ src/main/java/com/cg/zcript/AccountWapper.java
@@ -0,0 +1,12 @@
+package com.cg.zcript;
+
+public class AccountWapper {
+	public String name;
+	public String phone;
+	public AccountWapper(String name, String phone) {
+		super();
+		this.name = name;
+		this.phone = phone;
+	}
+	
+}
